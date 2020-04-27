765d22dcf68b6af5d9d73755d94639331568c3e1
marcelo oyaneder labarca <marcelo.oyaneder.l@gmail.com>
2020-04-27 16:23:38 -0400

    Create darwinizer.py

---
+++ scripts/darwinizer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pickle
+import pandas as pd
+
+#Abrir dict
+
+#Abrir archivo proveniente de refresh data base
+
+#Comparar columnas dwcorear
+
+#quiero guardar una lista con columnas de dwc del archivo y otras para visitor
+#  así ya esta predefinido cuando se abra nuevamente
+
+#Esta función debe ir en refresh data base o en darwinizer, 
+# mejor no ya que al guarar listas de columnas solo debo hacer esto una vez
+
+#Borar dynamic links user info y todos los dwc_terms
+
+#Herramienta para normalizar horas
+
+#Georreferenciación
+
+#Guardar df como csv y ese pasarlo para refreshdatabase
